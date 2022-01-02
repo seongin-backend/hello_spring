7089e69806d23195289409e2e494735c5e8af92e
seongin-backend <alterin@naver.com>
2022-01-02 23:27:17 +0900

    주석처리

--- src/main/java/seongin_backend/hello_spring/HelloSpringApplication.java
+++ src/main/java/seongin_backend/hello_spring/HelloSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args); //HelloSpringApplication 클래스 호출 - > @SpringBootApplication 어노테이션에 의해 서버 실행
 	}
 
 }
