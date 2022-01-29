e49a055d8d93424ff1e331b7730e7f969bf93994
seongin-backend <alterin@naver.com>
2022-01-29 23:58:01 +0900

    진행중

--- src/main/java/seongin_backend/hello_spring/controller/MemberController.java
+++ src/main/java/seongin_backend/hello_spring/controller/MemberController.java
@@ -1,9 +1,17 @@
 package seongin_backend.hello_spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import seongin_backend.hello_spring.service.MemberService;
 
 @Controller
 public class MemberController {
 
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 }
 
