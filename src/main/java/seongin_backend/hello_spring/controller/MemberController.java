1e3dbaec3cbac40a2718ebbf467615ea7cff6dc8
seongin-backend <alterin@naver.com>
2022-02-13 15:13:58 +0900

    회원가입, 조회 완성(repository 방식)

--- src/main/java/seongin_backend/hello_spring/controller/MemberController.java
+++ src/main/java/seongin_backend/hello_spring/controller/MemberController.java
@@ -2,8 +2,13 @@ package seongin_backend.hello_spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import seongin_backend.hello_spring.domain.Member;
 import seongin_backend.hello_spring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -18,6 +23,23 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/CreateMemberform";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
