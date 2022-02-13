d4819a98b273a5c04f188a015a5c47e3b7600751
seongin-backend <alterin@naver.com>
2022-02-13 16:53:15 +0900

    JDBC Repository 추가

--- src/main/java/seongin_backend/hello_spring/SpringConfig.java
+++ src/main/java/seongin_backend/hello_spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package seongin_backend.hello_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import seongin_backend.hello_spring.repository.JdbcMemberRepository;
 import seongin_backend.hello_spring.repository.MemberRepository;
 import seongin_backend.hello_spring.repository.MemoryMemberRepository;
 import seongin_backend.hello_spring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository()
+        return new JdbcMemberRepository(dataSource);
     }
+
 }
