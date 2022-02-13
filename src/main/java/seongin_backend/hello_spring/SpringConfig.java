2a7997c319b49cc5e795e0eaa478d3331d2a4318
seongin-backend <alterin@naver.com>
2022-02-13 23:21:12 +0900

    JdbcTemplate으로 저장소 변경

--- src/main/java/seongin_backend/hello_spring/SpringConfig.java
+++ src/main/java/seongin_backend/hello_spring/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import seongin_backend.hello_spring.repository.JdbcMemberRepository;
+import seongin_backend.hello_spring.repository.JdbcTemplateMemberRepository;
 import seongin_backend.hello_spring.repository.MemberRepository;
 import seongin_backend.hello_spring.repository.MemoryMemberRepository;
 import seongin_backend.hello_spring.service.MemberService;
@@ -28,7 +29,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         // return new MemoryMemberRepository()
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
